a068717f3c4ec3ba6ed410e38723d0b98e455d95
Cristian Crețu <crisemcr@gmail.com>
2023-08-24 13:27:26 +0300

    custom engine

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { InvoiceView } from "@/components/invoice-view";
+import { getInvoices } from "@/lib/actions";
+
 export default async function Home() {
+  const invoices = await getInvoices();
   return (
     <main className="mx-auto max-w-4xl w-[676px]">
-      Welcome to beacon.
+      maxuuuuuuuuuuuuu
+      {/* <InvoiceView invoice={invoices[0]} editable={true} /> */}
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
 }
 
 module.exports = nextConfig
